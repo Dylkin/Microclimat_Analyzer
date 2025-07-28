0bfe8c1f10b842ca7e72ad494ba9fa1a6fa13d16
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-07-28 22:39:15 +0300

    Исправить блокировку Sentry

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,19 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
+  server: {
+    port: 5173,
+    host: true
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['@sentry/browser', '@sentry/tracing']
+    }
   },
 });
