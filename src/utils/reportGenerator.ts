b22c99a7f796b39e3bd26b4d425d510b2d2e2047
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-07 00:49:39 +0300

    Updated reportGenerator.ts

--- src/utils/reportGenerator.ts
+++ src/utils/reportGenerator.ts
@@ -521,17 +521,17 @@ export class ReportGenerator {
       <wp:inline distT="0" distB="0" distL="0" distR="0">
         <wp:extent cx="5486400" cy="3200400"/>
         <wp:effectExtent l="0" t="0" r="0" b="0"/>
-        <wp:docPr id="1" name="График"/>
+        <wp:docPr id="1" name="Chart"/>
         <wp:cNvGraphicFramePr/>
         <a:graphic xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main">
           <a:graphicData uri="http://schemas.openxmlformats.org/drawingml/2006/picture">
             <pic:pic xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture">
               <pic:nvPicPr>
-                <pic:cNvPr id="1" name="График"/>
+                <pic:cNvPr id="1" name="Chart"/>
                 <pic:cNvPicPr/>
               </pic:nvPicPr>
               <pic:blipFill>
-                <a:blip r:embed="${chartRelId}" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"/>
+                <a:blip r:embed="rIdChart" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"/>
                 <a:stretch>
                   <a:fillRect/>
                 </a:stretch>
