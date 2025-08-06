a1046b52de73f11a01a0d0f012f39fbe0d6b1a41
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-07 00:40:16 +0300

    Updated reportGenerator.ts

--- src/utils/reportGenerator.ts
+++ src/utils/reportGenerator.ts
@@ -1299,11 +1299,11 @@ export class ReportGenerator {
     
     if (!relsFile) {
       // Создаем новый файл relationships с полной структурой Word документа
-      const newRels = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+        const newRelsContent = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <Relationships xmlns="http://schemas.openxmlformats.org/package/2006/relationships">
   <Relationship Id="rId1" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles" Target="styles.xml"/>
   <Relationship Id="rId2" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/settings" Target="settings.xml"/>
-  <Relationship Id="${chartRelId}" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="media/chart.png"/>
+  <Relationship Id="rIdChart" Type="http://schemas.openxmlformats.org/officeDocument/2006/relationships/image" Target="media/chart.png"/>
 </Relationships>`;
       zip.file(relsPath, newRels);
       this.chartRelationshipId = 'rId3';
