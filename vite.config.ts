e9cd9825c6d4e06cb8fdb1bbc8e7d7eab10af433
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-07 21:58:07 +0300

    Добавление глобального Buffer в Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   plugins: [react()],
   define: {
     global: 'globalThis',
-    'process.env': {}
+    'process.env': {},
+    Buffer: 'Buffer'
   },
   optimizeDeps: {
     include: [
