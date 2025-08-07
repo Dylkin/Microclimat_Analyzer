be13a8e346efe9f3638b90a2ca5d2c93e1a5e0b2
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-07 19:51:35 +0300

    Удалить код генерации отчета

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: {
-      vm: 'vm-browserify'
-    }
-  },
-  optimizeDeps: {
-    include: ['docx-templates']
-  },
   server: {
     port: 5173,
     host: true
   },
-  worker: {
-    format: 'es'
-  }
 })
