fe0d64e62ace4f46cc35971d806ca95a5ea18aff
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-07 20:05:46 +0300

    Updated index.html

--- vite.config.ts
+++ vite.config.ts
@@ -4,34 +4,6 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',
-    'process.env': {}
-  },
-  resolve: {
-    alias: {
-      'node:fs': 'fs',
-      'node:path': 'path-browserify',
-      'node:url': 'url',
-      'fs': 'memfs',
-      'path': 'path-browserify',
-      'stream': 'stream-browserify',
-      'util': 'util',
-      'buffer': 'buffer',
-      'process': 'process/browser'
-    }
-  },
-  optimizeDeps: {
-    include: [
-      'docx-templates',
-      'file-saver',
-      'buffer',
-      'process',
-      'stream-browserify',
-      'path-browserify',
-      'util'
-    ]
-  },
   server: {
     port: 5173,
     host: true
