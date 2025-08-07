022f3cfd99ba9a9e2bab5e6f14515dad8f17acb2
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-07 21:58:52 +0300

    Исправление глобального Buffer для браузера

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   define: {
     global: 'globalThis',
     'process.env': {},
-    Buffer: 'Buffer'
+    Buffer: 'globalThis.Buffer'
   },
   optimizeDeps: {
     include: [
