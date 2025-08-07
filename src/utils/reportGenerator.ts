637eea2610a06d96f4011b829385f4ac990d7929
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-07 22:32:17 +0300

    Исправление XML структуры DOCX файла

--- src/utils/reportGenerator.ts
+++ src/utils/reportGenerator.ts
@@ -241,8 +241,24 @@ export class ReportGenerator {
     if (!documentXml) return;
 
     let xmlContent = documentXml.asText();
+    
+    // Ищем плейсхолдер {chart} и заменяем его на изображение
+    // Важно: заменяем только содержимое внутри <w:t> тега, не создавая вложенную структуру
     const imageXml = this.createImageXml(relationshipId);
-    xmlContent = xmlContent.replace(/{chart}/g, imageXml);
+    
+    // Заменяем {chart} на пустую строку в текстовых узлах и добавляем изображение в параграф
+    xmlContent = xmlContent.replace(
+      /(<w:t[^>]*>)([^<]*{chart}[^<]*)(<\/w:t>)/g,
+      (match, openTag, textContent, closeTag) => {
+        // Удаляем {chart} из текста
+        const cleanText = textContent.replace(/{chart}/g, '');
+        if (cleanText.trim()) {
+          return `${openTag}${cleanText}${closeTag}</w:r>${imageXml}<w:r><w:t>`;
+        } else {
+          return `</w:r>${imageXml}<w:r><w:t>`;
+        }
+      }
+    );
     
     zip.file('word/document.xml', xmlContent);
   }
@@ -252,49 +268,48 @@ export class ReportGenerator {
    */
   private createImageXml(relationshipId: string): string {
     // Размеры в EMU (English Metric Units): 1 дюйм = 914400 EMU
-    const width = 5486400; // ~6 дюймов
-    const height = 3657600; // ~4 дюйма
+    // После поворота на 90° меняем местами ширину и высоту
+    const width = 3657600; // ~4 дюйма (было высотой до поворота)
+    const height = 5486400; // ~6 дюймов (было шириной до поворота)
     
     return `
-<w:p xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main">
-  <w:r>
-    <w:drawing>
-      <wp:inline xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" distT="0" distB="0" distL="0" distR="0">
-        <wp:extent cx="${width}" cy="${height}"/>
-        <wp:effectExtent l="0" t="0" r="0" b="0"/>
-        <wp:docPr id="1" name="Chart"/>
-        <wp:cNvGraphicFramePr>
-          <a:graphicFrameLocks xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" noChangeAspect="1"/>
-        </wp:cNvGraphicFramePr>
-        <a:graphic xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main">
-          <a:graphicData uri="http://schemas.openxmlformats.org/drawingml/2006/picture">
-            <pic:pic xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture">
-              <pic:nvPicPr>
-                <pic:cNvPr id="1" name="Chart"/>
-                <pic:cNvPicPr/>
-              </pic:nvPicPr>
-              <pic:blipFill>
-                <a:blip r:embed="${relationshipId}"/>
-                <a:stretch>
-                  <a:fillRect/>
-                </a:stretch>
-              </pic:blipFill>
-              <pic:spPr>
-                <a:xfrm>
-                  <a:off x="0" y="0"/>
-                  <a:ext cx="${width}" cy="${height}"/>
-                </a:xfrm>
-                <a:prstGeom prst="rect">
-                  <a:avLst/>
-                </a:prstGeom>
-              </pic:spPr>
-            </pic:pic>
-          </a:graphicData>
-        </a:graphic>
-      </wp:inline>
-    </w:drawing>
-  </w:r>
-</w:p>
+<w:r>
+  <w:drawing>
+    <wp:inline xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" distT="0" distB="0" distL="0" distR="0">
+      <wp:extent cx="${width}" cy="${height}"/>
+      <wp:effectExtent l="0" t="0" r="0" b="0"/>
+      <wp:docPr id="1" name="Chart"/>
+      <wp:cNvGraphicFramePr>
+        <a:graphicFrameLocks xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" noChangeAspect="1"/>
+      </wp:cNvGraphicFramePr>
+      <a:graphic xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main">
+        <a:graphicData uri="http://schemas.openxmlformats.org/drawingml/2006/picture">
+          <pic:pic xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture">
+            <pic:nvPicPr>
+              <pic:cNvPr id="1" name="Chart"/>
+              <pic:cNvPicPr/>
+            </pic:nvPicPr>
+            <pic:blipFill>
+              <a:blip r:embed="${relationshipId}" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"/>
+              <a:stretch>
+                <a:fillRect/>
+              </a:stretch>
+            </pic:blipFill>
+            <pic:spPr>
+              <a:xfrm>
+                <a:off x="0" y="0"/>
+                <a:ext cx="${width}" cy="${height}"/>
+              </a:xfrm>
+              <a:prstGeom prst="rect">
+                <a:avLst/>
+              </a:prstGeom>
+            </pic:spPr>
+          </pic:pic>
+        </a:graphicData>
+      </a:graphic>
+    </wp:inline>
+  </w:drawing>
+</w:r>
     `;
   }
 
