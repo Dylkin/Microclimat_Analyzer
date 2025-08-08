50a3ba9e1dd9ecdbc5058db154b30dd06701758c
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-08 19:48:48 +0300

    Исправление неиспользуемых импортов

--- src/components/DataVisualization.tsx
+++ src/components/DataVisualization.tsx
@@ -1,9 +1,6 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { BarChart, ArrowLeft } from 'lucide-react';
+import React from 'react';
 import { UploadedFile, MeasurementRecord } from '../types/FileData';
 import { TimeSeriesAnalyzer } from './TimeSeriesAnalyzer';
-import { ReportGenerator } from '../utils/reportGenerator';
-import { useAuth } from '../contexts/AuthContext';
 
 interface DataVisualizationProps {
   files: UploadedFile[];
