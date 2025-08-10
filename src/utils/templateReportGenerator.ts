0c29b284df2e17e4b2f3166d8340c2d6d617b356
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-10 21:38:06 +0300

    Fix require is not defined error

--- src/utils/templateReportGenerator.ts
+++ src/utils/templateReportGenerator.ts
@@ -1,6 +1,6 @@
 import PizZip from 'pizzip';
 import Docxtemplater from 'docxtemplater';
-const HtmlModule = require('docxtemplater-html-module');
+import HtmlModule from 'docxtemplater-html-module';
 
 export interface TemplateReportData {
   chartImageBlob: Blob;
