acc39b3cab052c1db7d7c17d1f88e926c4d64168
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-10 20:13:04 +0300

    Add resolve alias for docxtemplater-html-module

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'docxtemplater-html-module': 'docxtemplater-html-module/build/index.js'
+    }
+  },
   define: {
     global: 'globalThis',
     'process.env': {},
