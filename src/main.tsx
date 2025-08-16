2930ce5c056d42ab9498c3a0c274fa563252f85d
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-16 12:04:05 +0300

    Исправить ошибки с Buffer и docxtemplater-table-module

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { Buffer } from 'buffer'
+import * as buffer from 'buffer'
 
 // Make Buffer globally available
-window.Buffer = Buffer
+window.Buffer = buffer.Buffer
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
