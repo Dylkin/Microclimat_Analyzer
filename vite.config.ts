87cc8a8b9ed09343430cc5bfd44b0c226f2053f6
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-16 10:43:31 +0300

    Добавить resolve alias для docxtemplater-table-module

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'docxtemplater-table-module': 'docxtemplater-table-module/index.js'
+    }
+  },
   define: {
     global: 'globalThis',
     'process.env': {},
