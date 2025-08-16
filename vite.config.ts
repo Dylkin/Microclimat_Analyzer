b001c52172b750ef5609a8aa1f82f924d46613e1
Dylkin <142694991+Dylkin@users.noreply.github.com>
2025-08-16 10:38:43 +0300

    Удалить alias для docxtemplater-table-module

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'docxtemplater-table-module': 'docxtemplater-table-module/index.js'
-    }
-  },
   define: {
     global: 'globalThis',
     'process.env': {},
